a6a9ded8c8f0cec93b1a2db38c0878744716ac46
Nishkarsh Bains <nishcodingzone@gmail.com>
2025-04-06 23:47:45 +0530

    Update sparkles.tsx

--- src/components/ui/sparkles.tsx
+++ src/components/ui/sparkles.tsx
@@ -62,7 +62,7 @@ export const SparklesCore = (props: ParticlesProps) => {
           options={{
             background: {
               color: {
-                value: background || "#0d47a1",
+                value: background || "#000000",
               },
             },
             fullScreen: {
